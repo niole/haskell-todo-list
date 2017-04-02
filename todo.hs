fa506a09ccf6b939f9fed2a0486d6055be68f2f6
niole <niolenelson@gmail.com>
2017-04-01 21:27:05 -0700

    can render todos

--- todo.hs
+++ todo.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 import CodeWorld
 import qualified Data.Text as DT
-import Data.Text (Text, snoc)
+import Data.Text (Text, snoc, pack, append)
 
 {--
         add todos
@@ -9,23 +9,40 @@ import Data.Text (Text, snoc)
         extra: mark todo as completed
 --}
 
+isNonLetter :: Text -> Bool
+isNonLetter c = case c of
+        "Up" -> True
+        "Down" -> True
+        "Left" -> True
+        "Right" -> True
+        "Backspace" -> True
+        "Tab" -> True
+        "Enter" -> True
+        "Shift" -> True
+        "Ctrl" -> True
+        "Alt" -> True
+        "Esc" -> True
+        "PageUp" -> True
+        "PageDown" -> True
+        "End" -> True
+        "Home" -> True
+        "Insert" -> True
+        "Delete" -> True
+        "CapsLock" -> True
+        "NumLock" -> True
+        "ScrollLock" -> True
+        "PrintScreen" -> True
+        "Break" -> True
+        "Separator" -> True
+        "Cancel" -> True
+        "Help" -> True
+        _  -> False
+
 data State = State
         { inputString :: Text,
           todos :: [(Int, Text)]
         }
 
-{--
-        renders a todo item
---}
-renderTodo :: (Int, Text) -> Picture
-renderTodo (index, content) = (rectangle 2.0 1.0) & (text content)
-
-{--
-       renders the input with what has been typed so far
---}
-
-renderInput :: Text -> Picture
-renderInput input = text input
 
 {--
        handles backspaces and updates state accordingly
@@ -36,28 +53,89 @@ deleteChar (State inputString todos) = State (DT.init inputString) todos
 {--
        handles characters and spaces, updates state accordingly
 --}
-addChar :: Char -> State -> State
-addChar chr (State inputString todos) = State (snoc inputString chr) todos
-
-{--
-       handles enter
---}
+addChar :: Text -> State -> State
+addChar chr (State inputString todos) = State (append inputString chr) todos
 
 {--
        handles clicks
 --}
+handleClick :: Event -> State -> State
+handleClick (MousePress LeftButton (x, y)) (State content todos) = State content todos
+handleClick _ s = s
 
 {--
        handles key presses
 --}
+handleKeyPress :: Event -> State -> State
+handleKeyPress (KeyPress which) s
+        | isNonLetter which = handleNonLetter which s
+        | otherwise = handleLetter which s
 
 {--
-       handles clicks
+        handles events
 --}
+handleEvent :: Event -> State -> State
+handleEvent (KeyPress which) s = handleKeyPress (KeyPress which) s
+handleEvent (MousePress button point) s = handleClick (MousePress button point) s
+handleEvent _ s = s
 
 {--
-       removes clicked todo item
+       handles letter presses
 --}
+handleLetter :: Text -> State -> State
+handleLetter letter s = addChar letter s
 
+{--
+       handles non-letter presses
+--}
+handleNonLetter :: Text -> State -> State
+handleNonLetter "Enter" s = addTodo s
+handleNonLetter "Backspace" s = deleteChar s
+handleNonLetter _ s = s
+
+{--
+       removes todo
+--}
+removeTodo :: State -> State
+removeTodo (State inputContent todos) = State "" ( (0, inputContent):(incTodos todos) )
+
+{--
+       adds todo item by taking text out of input content state
+       and adds to front of todos
+--}
+addTodo :: State -> State
+addTodo (State inputContent todos) = State "" ( (0, inputContent):(incTodos todos) )
+
+decTodos :: [(Int, Text)] -> [(Int, Text)]
+decTodos todos = map (\((i, content)) -> (i-1, content)) todos
+
+incTodos :: [(Int, Text)] -> [(Int, Text)]
+incTodos todos = map (\((i, content)) -> (i+1, content)) todos
+
+renderTodos :: [(Int, Text)] -> Picture
+renderTodos todos =  foldl (\a -> \b -> a & (renderTodo b)) blank todos
+
+{--
+        renders a todo item
+--}
+renderTodo :: (Int, Text) -> Picture
+renderTodo (index, content) = translated 0.0 ((fromIntegral(index)*(-1.0)) - 1.0) $ (rectangle 2.0 1.0) & (text content)
+
+{--
+       renders the input with what has been typed so far
+--}
+renderInput :: Text -> Picture
+renderInput input = text input
+
+{--
+        renders whole todo list
+--}
+render :: State -> Picture
+render (State inputContent todos) = (renderInput inputContent) & (renderTodos todos)
+
+initialState :: State
+initialState = State "" []
+
+-- world -> (Double -> world -> world) -> (Event -> world -> world) -> (world -> Picture) -> IO ()
 main :: IO()
-main = putStrLn ""
+main = interactionOf initialState (\x -> \s -> s) handleEvent render
